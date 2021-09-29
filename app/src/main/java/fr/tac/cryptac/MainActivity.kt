9b2c4d307a989903811ee98e162d5f671a013541
larzul <hippolyte.larzul.etu@univ-lille.fr>
2021-09-29 16:30:54 +0200

    chore: initial commit

---
+++ app/src/main/java/fr/tac/cryptac/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.tac.cryptac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
